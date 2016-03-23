33c5a243cdbd719228abbd185991a049f8491a97
Stanislav Gatev <mail@s2gatev.com>
2016-03-23 20:39:47 +0200

    Add command-line args handler

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <cstring>
 
 #include "persistence/all.h"
 
-int main() {
-  Persistence::get("bmp")->load("something");
+using namespace std;
+
+void handle(Persistence::Image *&image, const char *command, const char *value) {
+  if (strcmp(command, "read") == 0) {
+    image = Persistence::get("bmp")->load("something");
+  } else if (strcmp(command, "apply") == 0) {
+  } else if (strcmp(command, "write") == 0) {
+  }
+}
+
+int main(int argc, char **argv) {
+  Persistence::Image *image;
+  for (int i = 1; i < argc; ++i) {
+    char *command = strtok(argv[i], ":"), *value = strtok(nullptr, ":");
+    handle(image, command, value);
+  }
 }
