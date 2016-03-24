d5077a73ae37cc1003d7e78f6d56e37d973ae293
Stanislav Gatev <mail@s2gatev.com>
2016-03-24 15:21:16 +0200

    Fix warnings

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
+WARN_FLAGS=-pedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wmissing-declarations -Wmissing-include-dirs -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-overflow=5 -Wswitch-default -Wundef -Werror -Wno-unused
+
 retouche: retouche.o
 	g++ -o retouche main.o image.o
 retouche.o: main.cpp
-	g++ -std=c++0x -c main.cpp model/image.cpp
+	g++ -std=c++0x -c $(WARN_FLAGS) main.cpp model/image.cpp
--- filter/blur_filter.h
+++ filter/blur_filter.h
@@ -8,17 +8,17 @@ class BlurFilter : public Operation {
 public:
     void apply(Model::Image *image) {
         float kernel[3][3] = {
-            0.0625, 0.125, 0.0625,
-            0.125, 0.25, 0.125,
-            0.0625, 0.125, 0.0625,
+            {0.0625, 0.125, 0.0625,},
+            {0.125, 0.25, 0.125,},
+            {0.0625, 0.125, 0.0625,}
         };
 
         Model::Color *reds = new Model::Color[image->width() * image->height()];
         Model::Color *greens = new Model::Color[image->width() * image->height()];
         Model::Color *blues = new Model::Color[image->width() * image->height()];
 
-        for(int x=0; x < image->width(); x++) {
-            for(int y = 0; y < image->height(); y++) {
+        for(unsigned int x=0; x < image->width(); x++) {
+            for(unsigned int y = 0; y < image->height(); y++) {
                 double red = 0.0, green = 0.0, blue = 0.0;
 
                 red += image->red(x, y)*kernel[1][1];
@@ -36,14 +36,14 @@ public:
                         blue += image->blue(x+1, y+1) * kernel[2][2];
                     }
 
-                    if (y - 1 > 0) {
+                    if (static_cast<int>(y) - 1 > 0) {
                         red += image->red(x+1, y-1) * kernel[2][0];
                         green += image->green(x+1, y-1) * kernel[2][0];
                         blue += image->blue(x+1, y-1) * kernel[2][0];
                     }
                 }
 
-                if (x - 1 > 0) {
+                if (static_cast<int>(x) - 1 > 0) {
                     red += image->red(x-1, y) * kernel[0][1];
                     green += image->green(x-1, y) * kernel[0][1];
                     blue += image->blue(x-1, y) * kernel[0][1];
@@ -54,7 +54,7 @@ public:
                         blue += image->blue(x-1, y+1) * kernel[0][2];
                     }
 
-                    if (y - 1 > 0) {
+                    if (static_cast<int>(y) - 1 > 0) {
                         red += image->red(x-1, y-1) * kernel[0][0];
                         green += image->green(x-1, y-1) * kernel[0][0];
                         blue += image->blue(x-1, y-1) * kernel[0][0];
@@ -67,20 +67,20 @@ public:
                     blue += image->blue(x, y+1) * kernel[1][2];
                 }
 
-                if (y - 1 > 0) {
+                if (static_cast<int>(y) - 1 > 0) {
                     red += image->red(x, y-1) * kernel[1][0];
                     green += image->green(x, y-1) * kernel[1][0];
                     blue += image->blue(x, y-1) * kernel[1][0];
                 }
 
-                reds[x * image->height() + y] = (Model::Color)(red);
-                greens[x * image->height() + y] = (Model::Color)(green);
-                blues[x * image->height() + y] = (Model::Color)(blue);
+                reds[x * image->height() + y] = static_cast<Model::Color>(red);
+                greens[x * image->height() + y] = static_cast<Model::Color>(green);
+                blues[x * image->height() + y] = static_cast<Model::Color>(blue);
             }
         }
 
-        for(int x=0; x < image->width(); x++) {
-            for(int y = 0; y < image->height(); y++) {
+        for(unsigned int x=0; x < image->width(); x++) {
+            for(unsigned int y = 0; y < image->height(); y++) {
                 image->setRed(x, y, reds[x * image->height() + y]);
                 image->setGreen(x, y, greens[x * image->height() + y]);
                 image->setBlue(x, y, blues[x * image->height() + y]);
