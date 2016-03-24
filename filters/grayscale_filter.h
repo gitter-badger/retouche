a85bc6dac22a19a44da39f5885ae6d47ff323485
Stanislav Gatev <mail@s2gatev.com>
2016-03-24 13:06:52 +0200

    Add grayscale filter implementation

--- filters/grayscale_filter.h
+++ filters/grayscale_filter.h
@@ -7,6 +7,17 @@ namespace Filters {
 class GrayscaleFilter : public Filter {
 public:
     void apply(Persistence::Image *image) {
+        for (unsigned int x = 0; x < image->getWidth(); x++) {
+            for (unsigned int y = 0; y < image->getHeight(); y++) {
+                unsigned char grayscaled = 0.21 * image->getRed(x, y) +
+                                           0.72 * image->getGreen(x, y) +
+                                           0.07 * image->getBlue(x, y);
+
+                image->setRed(x, y, grayscaled);
+                image->setGreen(x, y, grayscaled);
+                image->setBlue(x, y, grayscaled);
+            }
+        }
     }
 };
 }
